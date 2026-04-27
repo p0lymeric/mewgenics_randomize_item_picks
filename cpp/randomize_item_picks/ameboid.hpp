ecf86938d06cb7d5b7ca3ca93786e8aecfccbcc3
polymeric <229394044+p0lymeric@users.noreply.github.com>
2026-04-26 21:59:09 -0400

    1.0.1 release. - Exclude bat files from zip release to satisfy Nexus Mods' VirusTotal scans.

--- cpp/randomize_item_picks/ameboid.hpp
+++ cpp/randomize_item_picks/ameboid.hpp
@@ -16,7 +16,7 @@
 inline constexpr char MOD_AUTHOR[] = "polymeric";
 inline constexpr char MOD_NAME[] = "Randomize Item Picks";
 inline constexpr char MOD_URL[] = "https://www.nexusmods.com/games/mewgenics/mods/318\nhttps://github.com/p0lymeric/mewgenics_randomize_item_picks";
-inline constexpr char MOD_VERSION[] = "1.0.0";
+inline constexpr char MOD_VERSION[] = "1.0.1";
 
 // These addresses were extracted from Mewgenics.exe
 // The script under misc/find_rvas.py can help with recovering these addresses after a game update
